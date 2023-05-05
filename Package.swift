254a979bb64a2c8e21d8f103830ab9c0dfc27ba2
Antonijo Bezmalinovic <antonijo.bezmalinovic@infinum.hr>
2023-05-05 12:36:24 +0200

    update pacgage.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   products: [
   // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-        name: "I18n",
-        targets: ["I18n"]
+        name: "SwiftI18n",
+        targets: ["SwiftI18n"]
     )
   ],
   dependencies: [],
@@ -20,9 +20,8 @@ let package = Package(
   // Targets are the basic building blocks of a package. A target can define a module or a test suite.
   // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-        name: "I18n",
+        name: "SwiftI18n",
         path: "SwiftI18n",
-        exclude: ["Polyglot/PolyglotSwiftI18Extensions.swift"],
         sources: ["./Classes/Main/", "./Classes/Case/", "./Classes/Views/BaseViews/", "./Classes/Views/CaseViews/", "./Classes/Views/SwiftUI/"]
     )
   ]
